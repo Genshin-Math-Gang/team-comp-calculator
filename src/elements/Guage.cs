fb570ae628863c05813afadcfee1dbc95ff2f725
ndarwin314 <ndarwin@comcast.net>
2021-08-11 15:33:12 -0700

    initial guage commit

---
+++ src/elements/Guage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tcc.Elements
+{
+    public class Guage
+    {
+        private double[] guageValues;
+
+        public Guage(double[] guage_values)
+        {
+            this.guageValues = guage_values;
+            
+            // possibly do some validation later
+        }
+        
+        // change return type later
+        public int[] ElementApplied(int element_type, double guage_strength)
+        {
+            switch (element_type)
+            {
+                case Element.PHYSICAL
+            }
+        }
+    }
+}
---
+++ src/enemy/Enemy.cs
@@ -0,0 +1,13 @@
+﻿using Tcc.Stats;
+using Tcc.Elements;
+
+namespace Tcc.Eenemy
+{
+    public class Enemy
+    {
+        private Stats stats;
+        private Guage guage;
+    
+
+    }
+}
