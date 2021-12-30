3658a59968aa2562de2b143ca804bf40a9669f8a
Waymond9056 <raymondy1729@gmail.com>
2021-12-29 19:34:21 -0500

    Albedo Skill (scuffed) no burst ATM

--- src/units/Albedo.cs
+++ src/units/Albedo.cs
@@ -1,22 +1,19 @@
-//im gay for insane
- 
 using System;
 using System.Collections.Generic;
-using System.Runtime.InteropServices;
-using Tcc.Elements;
-using Tcc.Events;
-using Tcc.Stats;
-using Tcc.Weapons;
- 
+using Tcc.elements;
+using Tcc.events;
+using Tcc.stats;
+using Tcc.weapons;
+
 namespace Tcc.Units
 {
     public class Albedo: Unit
     {
         SnapshottedStats burstSnapshot;
-        public Albedo(int constellationLevel=0, string level="90", int autolevel=6, into skillLevel=6, int burstLevel=6):
+        public Albedo(int constellationLevel=0, string level="90", int autolevel=6, int skillLevel=6, int burstLevel=6):
             base("Albedo", level, constellationLevel, autoLevel, skillLevel, burstLevel, Element.GEO, WeaponType.SWORD)
         {
-            //cock
+            
             this.burstSnapshot = new SnapshottedStats(this, Types.BURST);
            
         }  
@@ -27,26 +24,51 @@ namespace Tcc.Units
             {
                 //Albedo MV for inital and placed skill are the same madge
                 SKillActivated(timestamp),
-                new Hit(timestamp+32/60, element, GetStatsPage, this, Types.SKILL,
-                new HitType(heavy:true), "Solar Isotoma Place"),
-                for(int i = 0, i < 15, i++){
-                    new Hit(timestamp+32/60+120*i, element, GetStatsPage, this. Types.SKILL,
-                    new HitType(heavy:true, "Solar Isotoma"))
-                }
+                new Hit(timestamp+32/60f, element, GetStatsPage, this, Types.SKILL,
+                    new HitType(heavy:true), "Solar Isotoma Place"),
+                new WorldEvent(timestamp, world =>
+                {
+                    foreach (var unit in world.GetUnits())
+                    {
+                        if (unit is not null) unit.DoDamageHook += SolarIsotoma;
+                    }
+                }),
+                new WorldEvent(timestamp + 30, world =>
+                {
+                    foreach (var unit in world.GetUnits())
+                    {
+                        if (unit is not null) unit.DoDamageHook -= SolarIsotoma;
+                    }
+                })   
             };
         }
+
+        public void SolarIsotoma(object? sender, Timestamp t)
+        {
+            
+            if (timestamp - lastIsotoma <= 1.99)
+            {
+                return;
+            }
+            newIsotoma = t;
+            lastIsotoma = newIsotoma;
+            t.World.AddWorldEvent(new Hit(newIsotoma, element, GetStatsPage, this, Type.SKILL,
+                new HitType(heavy:true), "Solar Isotoma Hit"));
+        }
  
+
+/*
         public List<WorldEvent> Burst(Timestamp timestamp)
         {
             return new List<WorldEvent>
             {
                 BurstActivated(timestamp),
                 burstSnapshot.Snapshot(timestamp),
-                new Hit(timestamp+96/60, element, GetStatsPage, this, Types.BURST,
+                new Hit(timestamp+96/60f, element, GetStatsPage, this, Types.BURST,
                 new HitType(heavy:true), "Rite of Progeniture: Tectonic Tide"),
                 //find fatal blossom frame
-                for (int i = 0, i < 7, i++){
-                    new Hit(timestamp+96/60+i/6, element, GetStatsPage, this, Types.BURST,
+                for (int i = 0; i < 7; i++){
+                    new Hit(timestamp+96/60+i/6f, element, GetStatsPage, this, Types.BURST,
                         new HitType(heavy:true), "Fatal Blossom")
                 }
                
@@ -54,7 +76,7 @@ namespace Tcc.Units
             };
         }
  
-   
+   */
  
         public override string ToString()
         {
